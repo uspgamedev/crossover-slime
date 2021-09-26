c91210272847bd9e4b370506a777bfe9f9879f67
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2021-09-25 21:13:25 -0300

    Fix dangling flame error

--- vfx/Flames.gd
+++ vfx/Flames.gd
@@ -3,5 +3,10 @@ extends Particles2D
 
 func _ready():
 	emitting = true
-	yield(get_tree().create_timer(2.0), "timeout")
+	#warning-ignore:return_value_discarded
+	get_tree() \
+		.create_timer(2.0) \
+		.connect("timeout", self, "_animation_ended", [], CONNECT_ONESHOT)
+
+func _animation_ended():
 	queue_free()
